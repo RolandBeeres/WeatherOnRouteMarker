807e0dced6206e0373b4f530a4ff2305d0920d4e
RolandBeeres <roland.beeres@gmail.com>
2018-07-09 10:51:44 +0200

    Adds application code

---
+++ scripts/WOR_mapsetup.js
@@ -0,0 +1,443 @@
+﻿
+//CONTENT BUILDING -----------------------------------------------------------------------------------------
+
+//Route start & end points - names: RouteStartDot, RouteEndDot
+RouteDotStartBorderStyle = function () { //route start end style - scales according to zoom level in USER INTERACTION section
+	return [new ol.style.Style({
+		zIndex: 2,
+		stroke: new ol.style.Stroke({
+			color: 'rgba(0,100,0,1)',
+			width: WOR.RouteDotRadius * 1.2
+		})
+	})];
+};
+RouteDotStartInnerStyle = function () { //route start end style - scales according to zoom level in USER INTERACTION section
+	return [new ol.style.Style({
+		zIndex: 2,
+		stroke: new ol.style.Stroke({
+			color: 'rgba(0,200,0,1)',
+			width: WOR.RouteDotRadius
+		})
+	})];
+};
+RouteDotEndStyle = function () { //route start end style - scales according to zoom level in USER INTERACTION section
+	return [new ol.style.Style({
+		stroke: new ol.style.Stroke({
+			color: 'rgba(200,0,0,1)',
+			width: WOR.RouteDotRadius
+		})
+	})];
+};
+
+var createRouteStartEndDots = function () {
+	var featureStartDotBorder = new ol.Feature({ // create the feature
+		geometry: new ol.geom.Circle([route.waypoints[0].lon, route.waypoints[0].lat]).transform('EPSG:4326', 'EPSG:3857'),
+		name: 'RouteStartDot',
+		lon: route.waypoints[0].lon,
+		lat: route.waypoints[0].lat
+	});
+	var featureStartDotInner = new ol.Feature({ // create the feature
+		geometry: new ol.geom.Circle([route.waypoints[0].lon, route.waypoints[0].lat]).transform('EPSG:4326', 'EPSG:3857'),
+		name: 'RouteStartDotInner',
+		lon: route.waypoints[0].lon,
+		lat: route.waypoints[0].lat
+	});
+	var feature2 = new ol.Feature({ // create the feature
+		geometry: new ol.geom.Circle([route.waypoints[route.waypoints.length - 1].lon, route.waypoints[route.waypoints.length - 1].lat]).transform('EPSG:4326', 'EPSG:3857'),
+		name: 'RouteEndDot',
+		lon: route.waypoints[route.waypoints.length - 1].lon,
+		lat: route.waypoints[route.waypoints.length - 1].lat
+	});
+	featureStartDotBorder.setId('RouteStartDot');
+	featureStartDotInner.setId('RouteStartDotInner');
+	feature2.setId('RouteEndDot');
+	featureStartDotBorder.setStyle(RouteDotStartBorderStyle());
+	featureStartDotInner.setStyle(RouteDotStartInnerStyle());
+	feature2.setStyle(RouteDotEndStyle());
+	return [featureStartDotBorder, featureStartDotInner, feature2];
+}
+
+
+
+//ROUTE DOTS (waypoints)
+RDstyle = function () { //route start end style - scales according to zoom level in USER INTERACTION section
+	return [new ol.style.Style({
+		stroke: new ol.style.Stroke({
+			color: 'rgba(0,0,0,0.5)',
+			width: 6
+		})
+	})];
+};
+RDAreastyle = function () { //route start end style - scales according to zoom level in USER INTERACTION section
+	return [new ol.style.Style({
+		stroke: new ol.style.Stroke({
+			color: 'rgba(200,0,0,0.2)',
+			width: WOR.RouteDotRadius
+		})
+	})];
+};
+var createRouteDots = function () {
+	var features = [];
+	if (route.waypoints.length > 2) {
+		for (var i = 1; i != route.waypoints.length - 1; i++) {
+			var feature1 = new ol.Feature({ // create the feature
+				geometry: new ol.geom.Circle([route.waypoints[i].lon, route.waypoints[i].lat]).transform('EPSG:4326', 'EPSG:3857'),
+				name: 'RouteDot',
+				routeDotNumber: i,
+				lon: route.waypoints[i].lon,
+				lat: route.waypoints[i].lat
+			});
+			var feature2 = new ol.Feature({ // create the feature
+				geometry: new ol.geom.Circle([route.waypoints[i].lon, route.waypoints[i].lat]).transform('EPSG:4326', 'EPSG:3857'),
+				name: 'RouteDotArea',
+				routeDotNumber: i,
+				lon: route.waypoints[i].lon,
+				lat: route.waypoints[i].lat
+			});
+
+			features.push(feature1)
+			features.push(feature2)
+		}
+	}
+	return features;
+}
+
+
+//ROUTE LEGS
+var createRouteLegs = function () {
+	var features = [];
+
+	lineStyle = function () { //route start end style - scales according to zoom level in USER INTERACTION section
+		return [new ol.style.Style({
+			stroke: new ol.style.Stroke({
+				color: 'rgba(200,0,0,0.7)',
+				width: 5
+			})
+		})];
+	};
+	if (route.waypoints.length > 2) {
+		for (var i = 0; i != route.waypoints.length - 1; i++) {
+			var coords = [];
+
+			if (i < route.waypoints.length) { //give angle with the feature so ship icon can be rotated to match
+				var start = [route.waypoints[i].lon, route.waypoints[i].lat];
+				var end = [route.waypoints[i + 1].lon, route.waypoints[i + 1].lat];
+				var rotation = turf.bearing(start, end);
+			}
+
+			coords.push([route.waypoints[i].lon, route.waypoints[i].lat]);
+			coords.push([route.waypoints[i + 1].lon, route.waypoints[i + 1].lat]);
+			var feature1 = new ol.Feature({ // create the feature
+				geometry: new ol.geom.LineString(coords).transform('EPSG:4326', 'EPSG:3857'),
+				name: 'RouteLeg',
+				routeLegNumber: i,
+				legrotation: rotation,
+
+			});
+			feature1.setId('RouteLeg' + i);
+			feature1.setStyle(lineStyle());
+
+			features.push(feature1)
+		}
+		return features;
+	}
+}
+
+//alert("Make lines pop back as individual features. See under map, remarked line.");
+
+
+
+//Weather On Route Marker (WORM) generator
+var retWORMWaveStyle = function (scale, wavedir, waveheight, markertext) {
+	if (!scale) scale = 1;
+	if (!wavedir) wavedir = 180;
+	wavedir += 45; //offset for icon
+	var useimage = (waveheight == "") ? 'images/WOR_backdropcircle_nowave.png' : 'images/WOR_backdropcircle.png';
+	if (markertext == "nodata") useimage = "images/WOR_nodata.png";
+
+
+	if (!waveheight || waveheight==0) waveheight = "";
+	var radOff = 0.47; //text offset in radians for current and wave indicator
+	var WORMWaveStyle = new ol.style.Style({
+		zIndex: 50,
+		image: new ol.style.Icon({
+			opacity: 0.75,
+			rotation: degToRad(wavedir), //wavepointer is pointing lowerright
+			anchor: [(0.5), (0.5)],
+			anchorXUnits: 'fraction',
+			anchorYUnits: 'fraction',
+			src: useimage,
+			scale: (0.5 * scale)
+		}),
+		text: new ol.style.Text({
+			font: '12px helvetica,sans-serif',
+			text: ('' + waveheight),
+			offsetX: calcSinCosFromAngle('x', degToRad(wavedir) + radOff, (40 * scale)),
+			offsetY: calcSinCosFromAngle('y', degToRad(wavedir) + radOff, (40 * scale)),
+			scale: (1 * scale),
+			fill: new ol.style.Fill({
+				color: '#000'
+			}),
+			stroke: new ol.style.Stroke({
+				color: '#fff',
+				width: 1
+			})
+		})
+	});
+	return WORMWaveStyle;
+}
+
+var retWORMCurrentStyle = function (scale, currdir, currstr, markertext) {
+	if (!scale) scale = 1;
+	if (!currdir) currdir = 180;
+	currdir += 45; //offset for icon
+	(!currstr) ? currstr = "" : currstr * 1.9438444924574; // make "" if nothing, or meter/sec to knots.
+	var useimage = 'images/WOR_innercircle.png';
+	if (markertext == "nodata") useimage = "images/emptyimage.png";
+
+	if (!currstr || currstr == 0) currstr = "";
+	var radOff = 0.25; //text offset in radians for current and wave indicator
+	var WORMCurrentStyle = new ol.style.Style({
+		zIndex: 51,
+		image: new ol.style.Icon({
+			opacity: (currstr!="")?1:0,
+			rotation: degToRad(currdir), //currentpointer is pointing lowerright
+			anchor: [0.5, 0.5],
+			anchorXUnits: 'fraction',
+			anchorYUnits: 'fraction',
+			src: useimage, //needs path
+			scale: (0.5 * scale)
+		}),
+		text: new ol.style.Text({
+			font: '10px helvetica,sans-serif',
+			text: ('' + currstr),
+			offsetX: calcSinCosFromAngle('x', degToRad(currdir) + radOff, (18 * scale)),
+			offsetY: calcSinCosFromAngle('y', degToRad(currdir) + radOff, (18 * scale)),
+			scale: (1 * scale),
+			fill: new ol.style.Fill({
+				color: '#000'
+			}),
+			stroke: new ol.style.Stroke({
+				color: '#fff',
+				width: 1
+			})
+		})
+	});
+	return WORMCurrentStyle;
+}
+
+
+var retWORMWindStyle = function (scale, winddir, windstr, markertext, wavedir) { //windstr is m/s - wavedir is needed to make offset greater if pointing south so text doesnt overlap. 
+	if (!scale) scale = 1;
+	var waypointtextoffset = 46;
+	if (!winddir) winddir = 180; //default north
+	(!windstr) ? windstr = 1 : windstr * 1.9438444924574; // make 1 knot if nothing, or meter/sec to knots.
+	var markerImageNamePath = "images/wind/";
+
+
+	//Determine wind marker image to display
+	if (windstr < 1.9){
+		markerImageNamePath += 'mark000.png';
+	} else if (windstr >= 2 && windstr < 7.5) {
+		markerImageNamePath += 'mark005.png';
+	} else if (windstr >= 7.5 && windstr < 12.5) {
+		markerImageNamePath += 'mark010.png';
+	} else if (windstr >= 12.5 && windstr < 17.5) {
+		markerImageNamePath += 'mark015.png';
+	} else if (windstr >= 17.5 && windstr < 22.5) {
+		markerImageNamePath += 'mark020.png';
+	} else if (windstr >= 22.5 && windstr < 27.5) {
+		markerImageNamePath += 'mark025.png';
+	} else if (windstr >= 27.5 && windstr < 32.5) {
+		markerImageNamePath += 'mark030.png';
+	} else if (windstr >= 32.5 && windstr < 37.5) {
+		markerImageNamePath += 'mark035.png';
+	} else if (windstr >= 37.5 && windstr < 42.5) {
+		markerImageNamePath += 'mark040.png';
+	} else if (windstr >= 42.5 && windstr < 47.5) {
+		markerImageNamePath += 'mark045.png';
+	} else if (windstr >= 47.5 && windstr < 52.5) {
+		markerImageNamePath += 'mark050.png';
+	} else if (windstr >= 52.5 && windstr < 57.5) {
+		markerImageNamePath += 'mark055.png';
+	} else if (windstr >= 57.5 && windstr < 62.5) {
+		markerImageNamePath += 'mark060.png';
+	} else if (windstr >= 62.5 && windstr < 67.5) {
+		markerImageNamePath += 'mark065.png';
+	} else if (windstr >= 67.5 && windstr < 72.5) {
+		markerImageNamePath += 'mark070.png';
+	} else if (windstr >= 72.5 && windstr < 77.5) {
+		markerImageNamePath += 'mark075.png';
+	} else if (windstr >= 77.5 && windstr < 82.5) {
+		markerImageNamePath += 'mark080.png';
+	} else if (windstr >= 82.5 && windstr < 87.5) {
+		markerImageNamePath += 'mark085.png';
+	} else if (windstr >= 87.5 && windstr < 92.5) {
+		markerImageNamePath += 'mark090.png';
+	} else if (windstr >= 92.5 && windstr < 97.5) {
+		markerImageNamePath += 'mark095.png';
+	} else if (windstr >= 97.5) {
+		markerImageNamePath += 'mark100.png';
+	}
+
+	//move the text a bit lower if the wavearrow points down
+	if ((wavedir < 55 && wavedir > 0) || (wavedir > 305)) { 
+		waypointtextoffset = 56;
+	}
+
+	var useimage = markerImageNamePath;
+	if (markertext == "nodata") {
+		useimage = "images/emptyimage.png";
+		markertext = "";
+	}
+
+
+	var WORMWindStyle = new ol.style.Style({
+		zIndex: 52,
+		image: new ol.style.Icon(({
+			opacity: 1,
+			rotation: degToRad(winddir), //windpointer is straight is pointing straight down
+			anchor: [(0.52), (0.25)],
+			anchorXUnits: 'fraction',
+			anchorYUnits: 'fraction',
+			src: useimage, //needs path and windstr to paint correct arrow
+			scale: (0.80 * scale)
+		})),
+		text: new ol.style.Text({
+			font: 'bold 12px helvetica,sans-serif',
+			text: "" + markertext,
+			offsetX: 0,
+			offsetY: waypointtextoffset * scale,
+			scale: (1 * scale),
+			fill: new ol.style.Fill({
+				color: '#000'
+			}),
+			stroke: new ol.style.Stroke({
+				color: '#fff',
+				width: 1
+			})
+		})
+	});
+	return WORMWindStyle;
+}
+
+
+
+//creates a weather marker
+var generateWORM = function (identifier, type, lon, lat, scale, winddir, windstr, currdir, currstr, wavedir, waveheight, markertext) { //type is given so it can be styled.
+	if (!lon || !lat) { lon = 0; lat = 0; }
+
+	//display errormarker if no data
+	if (winddir == 0 && windstr == 0 && currdir == 0 && currstr == 0 && wavedir == 0 && waveheight == 0 && markertext == 0) {
+		markertext = "nodata"; //styling takes care of it from here
+	}
+
+
+	//WAVEARROW
+	var iconFeature = new ol.Feature({ 
+		geometry: new ol.geom.Point([lon, lat]).transform('EPSG:4326', 'EPSG:3857'),
+		name: 'WOR_wavemarker',
+		type: type,
+		identifier: identifier,
+		src: 'images/WOR_vessel_backdropcircle.png',
+	});
+	iconFeature.setStyle(retWORMWaveStyle(scale, wavedir, waveheight, markertext)); //change stylíng
+	iconFeature.setId(type + '_wavemarker');
+
+	//CURRENTARROW
+	var iconFeature2 = new ol.Feature({
+		geometry: new ol.geom.Point([lon, lat]).transform('EPSG:4326', 'EPSG:3857'),
+		name: 'WOR_currentmarker',
+		type: type,
+		identifier: identifier,
+		src: 'images/WOR_innercircle.png',
+	});
+	iconFeature2.setStyle(retWORMCurrentStyle(scale, currdir, currstr, markertext));
+	iconFeature2.setId(type + '_currentmarker');
+
+	//WINDARROW
+	var iconFeature3 = new ol.Feature({
+		geometry: new ol.geom.Point([lon, lat]).transform('EPSG:4326', 'EPSG:3857'),
+		name: 'WOR_windmarker',
+		type: type,
+		identifier: identifier,
+		src: 'images/wind/mark005.png',
+	});
+	iconFeature3.setStyle(retWORMWindStyle(scale, winddir, windstr, markertext, wavedir));
+	iconFeature3.setId(type + '_windmarker');
+
+	return [iconFeature, iconFeature2, iconFeature3];
+}
+
+
+
+
+var retLoadingIconStyle = function () {
+	var WORMLoadinStyle = new ol.style.Style({
+		image: new ol.style.Icon(({
+			opacity: 1,
+			scale:0.3,
+			anchor: [(0.5), (0.5)],
+			anchorXUnits: 'fraction',
+			anchorYUnits: 'fraction',
+			src: 'images/loadingicon.png',
+		}))
+	});
+	return WORMLoadinStyle;
+}
+
+
+
+var retLoadingIcon = function (lonlat) {
+	var iconFeature = new ol.Feature({
+		geometry: new ol.geom.Point(lonlat).transform('EPSG:4326', 'EPSG:3857'),
+		name: 'WOR_loadingicon',
+		src: 'images/loading.gif',
+	});
+	iconFeature.setStyle(retLoadingIconStyle()); //generated style
+	iconFeature.setId("WORMLoadingIcon");
+	return [iconFeature];
+
+}
+
+
+
+
+//END WEATHER ON ROUTE MARKER (WORM) *****************************************************
+
+
+var generateRouteLayer = function () {
+	var point = new ol.geom.Point([0, 0]).transform('EPSG:4326', 'EPSG:3857'); //transform to EPSG:3857
+	var feature = new ol.Feature({ // create the feature
+		geometry: point,
+	});
+	var pointStyle = new ol.style.Style({
+		stroke: new ol.style.Stroke({
+			color: 'rgba(255,0,0,0)',
+			width: 200
+		})
+	});
+	var source = new ol.source.Vector({
+		features: [feature]
+	});
+	var routeLayer = new ol.layer.Vector({
+		source: source,
+		style: [pointStyle],
+		name: 'routeLayer'
+	});
+	return routeLayer;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ scripts/WOR_workfunctions.js
@@ -0,0 +1,160 @@
+﻿
+
+// convert degrees to radians
+function degToRad(deg) {
+	return deg * Math.PI * 2 / 360;
+}
+
+
+// convert radians to degrees
+function radToDeg(rad) {
+	return rad * 360 / (Math.PI * 2);
+}
+
+function getRandomInt(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function calcSinCosFromAngle(xy, angle, radius) { //requires ('x' or 'y'), angle in degrees and radius in px.
+	var SinCos;
+	if (xy == 'x') SinCos = radius * Math.cos(angle); // Calculate the x position of the element.
+	if (xy == 'y') SinCos = radius * Math.sin(angle); // Calculate the y position of the element.
+	return SinCos;
+}
+
+function getAngleFromPoints(cx, cy, ex, ey) { //returns the angle of a line fom 2 points xy - xy -> careful if using negative numbers. This is for the Baltic, which is in positive.
+	var dy = ey - cy;
+	var dx = ex - cx;
+	var theta = Math.atan2(dy, dx); // range (-PI, PI]
+	theta *= 180 / Math.PI; // rads to degs, range (-180, 180]
+	//if (theta < 0) theta = 360 + theta; // range [0, 360)
+	return theta;
+}
+
+var getDistanceFromCoords = function (lonlat1, lonlat2) { //returns distance in nautical miles between 2 points. format: lonlatX=[xx.xx,xx.xx]
+	var wgs84Sphere = new ol.Sphere(6378137); //define spherical math layout
+	var length;
+	var coordinates = []; coordinates.push(lonlat1); coordinates.push(lonlat2);
+	length = 0;
+	var sourceProj = map.getView().getProjection();
+	for (var i = 0, ii = coordinates.length - 1; i < ii; ++i) {
+		var c1 = ol.proj.transform(coordinates[i], sourceProj, 'EPSG:3857');
+		var c2 = ol.proj.transform(coordinates[i + 1], sourceProj, 'EPSG:3857');
+		length += wgs84Sphere.haversineDistance(c1, c2);
+	}
+	var output = length * 0.539956803;
+	output = (Math.round(output / 1000 * 100) / 100)
+	return output;
+};
+
+
+function calculateAndSaveDistanceBetweenRouteMarkers() { //saves distance between markers to handle zooming behaviour of route weathermarkers
+	var distdebug = "";
+
+	for (var i = 0; i != route.waypoints.length-1; i++) {
+
+		var thiswaypoint = [route.waypoints[i].lon, route.waypoints[i].lat],
+			nextwaypoint = [route.waypoints[i+1].lon, route.waypoints[i+1].lat];
+
+		route.scheduleElement[i].nextwaypointdistance = getDistanceFromCoords(thiswaypoint, nextwaypoint);
+	}
+}
+
+
+function updateRouteWORMFunction(routemarkernumber) { //create route weather marker when data is available.
+
+	var winddirection = route.scheduleElement[routemarkernumber].winddirection;
+	var windspeed = route.scheduleElement[routemarkernumber].windspeed;
+	var currentdirection = route.scheduleElement[routemarkernumber].currentdirection;
+	var currentspeed = route.scheduleElement[routemarkernumber].currentspeed;
+	var wavedirection = route.scheduleElement[routemarkernumber].wavedirection;
+	var waveheight = route.scheduleElement[routemarkernumber].waveheight;
+
+	var timehours = (route.scheduleElement[routemarkernumber].eta.split("T")[1]).split(".")[0]; //get hours & minutes from 2017-04-19T11:00:01.000Z
+	timehours = timehours.substring(0,timehours.length-3);
+
+	var markertext = retDayFromRTZ(route.scheduleElement[routemarkernumber].eta) + "\n" + timehours + " UTC";
+	if (!control_displaymarkertext) markertext = "";
+	//adds a new clickmarker with updated info
+	mapSource.addFeatures(generateWORM('ROUTEWEATHERMARKER', 'routeweathermarker_' + routemarkernumber, route.waypoints[routemarkernumber].lon, route.waypoints[routemarkernumber].lat, control_scale, winddirection, windspeed, currentdirection, currentspeed, wavedirection, waveheight, markertext));
+	
+}
+
+
+
+//test route weathermarkers for overlapping on zoom, remove the lowest overlapping one in order from start to end
+function cleanWeatherMarkersOverlapping() {
+	function hideshowmarkerswithindistance(showhidedistance) {
+		var totalDistance = 0;
+		for (var i = 0; i != route.scheduleElement.length - 1; i++) {//loop through all waypoints, remove any that are closer than (distance)
+			for (var y = i; y != route.scheduleElement.length - 1; y++) {
+				totalDistance = parseFloat(route.scheduleElement[i].nextwaypointdistance) + parseFloat(route.scheduleElement[i + 1].nextwaypointdistance); //add up distances
+				if (showhidedistance > totalDistance) {
+					try { //remove the marker
+						mapSource.removeFeature(mapSource.getFeatureById("routeweathermarker_" + (i) + "_wavemarker"));
+						mapSource.removeFeature(mapSource.getFeatureById("routeweathermarker_" + (i) + "_currentmarker"));
+						mapSource.removeFeature(mapSource.getFeatureById("routeweathermarker_" + (i) + "_windmarker"));
+					} catch (ExceptionNoFeature) { }
+					route.scheduleElement[i].zoomdisplay = mapZoomLevel; //save at which zoomlevel to show this marker again
+				}
+			}
+			//add markers that are within acceptable distance again on zoom change
+			if (route.scheduleElement[i].zoomdisplay > 0 && mapZoomLevel > (route.scheduleElement[i].zoomdisplay)) {
+				updateRouteWORMFunction(i);
+				route.scheduleElement[i].zoomdisplay = 0; //reset marker zoom behaviour
+			}
+		}
+	}
+	var showhidedistance = 0;
+	switch (mapZoomLevel) {
+
+		case 11:
+			showhidedistance = 0.9;
+			break;
+		case 10:
+			showhidedistance = 1.8;
+			break;
+		case 9:
+			showhidedistance = 3.5;
+			break;
+		case 8:
+			showhidedistance = 6;
+			break;
+		case 7:
+			showhidedistance = 11;
+			break;
+		case 6:
+			showhidedistance = 22;
+			break;
+		case 5:
+			showhidedistance = 33;
+			break;
+		case 4:
+			showhidedistance = 66;
+			break;
+	}
+	hideshowmarkerswithindistance(showhidedistance);
+}
+
+
+
+function rendertimeformat(tm) {
+   console.log("tm:",tm);
+	if (tm && tm != "") return tm.toISOString().substring(0, tm.toISOString().length - 4) + "00Z";
+}
+
+function retDayFromRTZ(tm) { //returns which day it is from a timestamp
+	var days,
+		tm = tm.split("T")[0],
+		tmnew = new Date(tm);
+	days = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+	return days[tmnew.getUTCDay()];
+}
+
+
+
+
+
+
